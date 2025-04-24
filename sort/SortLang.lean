f60f0d60f162f4789d5c81f62ff94ca2d3e5bfe3
fduxiao <9294979+fduxiao@users.noreply.github.com>
2025-04-24 18:04:48 -0400

    add swap

--- sort/SortLang.lean
+++ sort/SortLang.lean
@@ -107,10 +107,12 @@ inductive Imp.BigStep: Imp -> State -> State -> Prop where
   | BAsgn {st: State} {a n x}:
     st.aeval a = n ->
     (Imp.Asgn x a).BigStep st st![ x => n; st ]
-  | BSwap {st: State} {x1 x2: VarIndex} {n1 n2}:
-    st.aeval x1 = n1 ->
-    st.aeval x1 = n2 ->
-    (Imp.Swap x1 x2).BigStep st st![ x1 => n2; x2 => n1; st]
+  | BSwap {st: State} {x1 x2: VarIndex} {a1 a2 n1 n2}:
+    st.aeval x1 = a1 ->
+    st.aeval x1 = a2 ->
+    st.aeval a1 = n1 ->
+    st.aeval a2 = n2 ->
+    (Imp.Swap x1 x2).BigStep st st![ a1 => n2; a2 => n1; st]
   | BSeq {c1 c2: Imp} {st1 st2 st3: State}:
     c1.BigStep st1 st2 ->
     c2.BigStep st2 st3 ->
@@ -140,6 +142,7 @@ syntax "<{" imp "}>": term
 
 syntax:100 "skip": imp
 syntax:100 "#[" term "]" ":=" aexp:15 : imp
+syntax:100 "swap" "#[" term "]" "#[" term "]": imp
 syntax:100 "(" imp ")" : imp
 syntax:10 imp ";" imp : imp
 syntax:11 "if" bexp:5 "then" imp:5 "else" imp:5 "end": imp
@@ -150,6 +153,7 @@ syntax term: imp
 macro_rules
   | `(<{ skip }>) => `(Imp.Skip)
   | `(<{ #[$t] := $y:aexp }>) => `(Imp.Asgn $t [AExp|$y])
+  | `(<{ swap #[$t1] #[$t2]}>) => `(Imp.Swap $t1 $t2)
   | `(<{ ( $x:imp ) }>) => `(<{ $x }>)
   | `(<{ $c1 ; $c2 }>) => `(Imp.Seq <{$c1}> <{$c2}>)
   | `(<{ if $b then $t else $f end }>) => `(Imp.If [BExp|$b] <{$t}> <{$f}>)
@@ -160,13 +164,14 @@ namespace Playground
 
 -- #[0] is the input
 -- sum from 1 to #[0]
-example: Imp := <{
+example := <{
   #[2] := 0;
   #[1] := 0;
   while (~ #[2] == #[0]) do
     #[1] := #[1] + #[2];
     #[2] := #[2] + 1
-  end
+  end;
+  swap #[0] #[1]
 }>
 
 end Playground
